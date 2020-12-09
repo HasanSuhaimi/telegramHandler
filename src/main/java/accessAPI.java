aa5b4d454ec38274413db6a59428df5fb93db52b
Hasan Suhaimi <srip_arif@yahoo.com.my>
2020-12-10 02:23:09 +0800

    Update accessAPI.java

--- src/main/java/accessAPI.java
+++ src/main/java/accessAPI.java
@@ -2,6 +2,7 @@ import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.*;
 import com.google.api.client.json.JsonFactory;
@@ -17,6 +18,7 @@ import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,83 +28,10 @@ import java.lang.Object;
 
 public class accessAPI {
 
-
-    public static void main(String[] args) throws Exception {
-
-        /** OAuth 2 scope. */
-        String SCOPE = "https://www.googleapis.com/auth/spreadsheets";
-
-        /** OAuth 2 client ID and Secret. */
-        String CLIENT_ID = "557076360085-7g47kdj1gkmc3s8o3me7bvun11bilm3u.apps.googleusercontent.com";
-        String CLIENT_SECRET = "oVOzNcmWaQcBUDcphrpl08gm";
-
-        /** Global instance of the HTTP transport. */
-        HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-
-        /** Global instance of the JSON factory. */
-        JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-        String spreadsheetId = "14ql0geK26IPAvj8KlD-Ljtv9IqiiRR7WsVEKFSvSEYY";
-        String range = "Extraction";
-
-        ValueRange requestBody = new ValueRange();
-        requestBody.setRange("A1");
-
-        List<List<Object>> values = Arrays.asList(
-                Arrays.asList(
-                        // Cell values ...
-                        "test","we will see","B","O","O","B","available"
-                )
-                // Additional rows ...
-        );
-
-        requestBody.setValues(values);
-
-        try {
-            final Credential credential = new accessAPI().authorize(HTTP_TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPE);
-
-            Sheets sheetsService = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                    .setApplicationName("appendSheet")
-                    .build();
-
-            Sheets.Spreadsheets.Values.Get request =
-                    sheetsService.spreadsheets().values().get(spreadsheetId, range);
-
-            ValueRange response = request.execute();
-
-            // TODO: Change code below to process the `response` object:
-            System.out.println(response);
-
-            //Sheets.Spreadsheets.Values.Update
-            Sheets.Spreadsheets.Values.Append postRequest =
-                    sheetsService.spreadsheets().values().append(spreadsheetId, "A1", requestBody);
-
-            postRequest.setValueInputOption("USER_ENTERED");
-
-            AppendValuesResponse postResponse = postRequest.execute();
-
-            // TODO: Change code below to process the `response` object:
-            System.out.println(postResponse);
-
-            // Success!
-            return;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-        System.exit(1);
-
-    }
-
     public void runAppend (String a, String b, String c, String d, String e, String f) throws Exception {
 
         /** OAuth 2 scope. */
         String SCOPE = "https://www.googleapis.com/auth/spreadsheets";
-
-        /** OAuth 2 client ID and Secret. */
-        String CLIENT_ID = "557076360085-7g47kdj1gkmc3s8o3me7bvun11bilm3u.apps.googleusercontent.com";
-        String CLIENT_SECRET = "oVOzNcmWaQcBUDcphrpl08gm";
 
         /** Global instance of the HTTP transport. */
         HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -124,8 +53,10 @@ public class accessAPI {
                 // Additional rows ...
         );
 
-        final Credential credential = new accessAPI().authorize(HTTP_TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPE);
-
+        //final Credential credential = new accessAPI().authorize(HTTP_TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPE);
+        GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream("src/main/java/access.json"))
+            .createScoped(Collections.singleton(SCOPE));
+        
         requestBody.setValues(values);
 
         Sheets sheetsService = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
