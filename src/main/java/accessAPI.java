7d43db3d89e60a41a4ead536b7e1d610d4b2594e
Hasan Suhaimi <srip_arif@yahoo.com.my>
2020-12-26 10:47:59 +0800

    Update accessAPI.java
    add sort sheet function

--- src/main/java/accessAPI.java
+++ src/main/java/accessAPI.java
@@ -103,7 +103,6 @@ public class accessAPI {
         //remove all whitespace
         range = range.replaceAll("\\s+","");
 
-        //final Credential credential = new accessAPI().authorize(HTTP_TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET, SCOPE);
         GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream("/home/dev/telegramHandler/telegramHandler/src/main/java/access.json"))
                 .createScoped(Collections.singleton(SCOPE));
 
@@ -119,6 +118,74 @@ public class accessAPI {
         // TODO: Change code below to process the `response` object:
         System.out.println(clearResponse);
 
+    }
+    public void sortSheet (String range) throws Exception {
+
+        String SCOPE = "https://www.googleapis.com/auth/spreadsheets";
+
+        /** Global instance of the HTTP transport. */
+        HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        /** Global instance of the JSON factory. */
+        JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+        String spreadsheetId = "14ql0geK26IPAvj8KlD-Ljtv9IqiiRR7WsVEKFSvSEYY";
+
+        range = range.replaceAll("\\s+","");
+        
+        GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream("/home/dev/telegramHandler/telegramHandler/src/main/java/access.json"))
+                .createScoped(Collections.singleton(SCOPE));
+
+        Sheets sheetsService = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName("appendSheet")
+                .build();
+
+        //get the sheet values from spreadsheet.values api
+        Sheets.Spreadsheets.Values.Get getRequest =
+                sheetsService.spreadsheets().values().get(spreadsheetId, range);
+        ValueRange response = getRequest.execute();
+
+        // get the last row
+        System.out.println(response.getValues().size());
+        int totalRow = response.getValues().size();
+
+        //get the sheet properties from spreadsheets.get api
+        List<String> ranges = new ArrayList<>();
+        ranges.add(range);
+        Sheets.Spreadsheets.Get propRequest = sheetsService.spreadsheets().get(spreadsheetId);
+        propRequest.setRanges(ranges);
+
+        Spreadsheet propResponse = propRequest.execute();
+        System.out.println(propResponse.getSheets().get(0).getProperties().getSheetId());
+        int sheetID = propResponse.getSheets().get(0).getProperties().getSheetId();
+
+        //start the batch update to sort
+        List<Request> requests = new ArrayList<>();
+
+        List<SortSpec> sortSpecs = new ArrayList<>();
+        sortSpecs.add(new SortSpec().setDimensionIndex(0).setSortOrder("ASCENDING"));
+
+        //request to sort the range specify using the sheetID for data range A1:J27
+        requests.add(new Request()
+                .setSortRange(new SortRangeRequest()
+                        .setRange(new GridRange()
+                                .setSheetId(sheetID)
+                                .setStartRowIndex(2)
+                                .setEndRowIndex(totalRow)
+                                .setStartColumnIndex(0)
+                                .setEndColumnIndex(10))
+                        .setSortSpecs(sortSpecs)));
+
+        BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest();
+        requestBody.setRequests(requests);
+
+        Sheets.Spreadsheets.BatchUpdate batchRequest =
+                sheetsService.spreadsheets().batchUpdate(spreadsheetId, requestBody);
+
+        BatchUpdateSpreadsheetResponse batchResponse = batchRequest.execute();
+
+        // TODO: Change code below to process the `response` object:
+        System.out.println(batchResponse);
     }
 
     /** Authorizes the installed application to access user's protected data. */
